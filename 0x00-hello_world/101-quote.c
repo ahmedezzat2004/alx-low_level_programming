211be0f1872febba858e62c880949ab2b0175942
Ahmed ezzat <azt299712@gmail.com>
2023-08-13 11:01:33 -0700

    Hello C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,12 @@
  *   ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: 1 (error).
- * */
+ **/
 
 int main(void)
-{
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write (1.quo, 59);
-return (1);
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo, 59);
+	return (1);
 }
 
