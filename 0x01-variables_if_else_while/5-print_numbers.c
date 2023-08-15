2b47112cab66fb570250343d7a6b12c3a9c3b61d
Ahmed ezzat <azt299712@gmail.com>
2023-08-14 17:59:56 -0700

    C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - start main
+* Description: write alphabet
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int a;
+	for (a = 0 ; a < 11 ; a++)
+	{
+		putchar(a);
+	}
+        putchar('\n');
+	return (0);
+}
